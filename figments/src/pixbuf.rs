c40dbce3be90df30fb939e9a7b92080dae255d6b
Victoria Fischer <tdfischer@hackerbots.net>
2025-10-02 14:38:02 +0200

    pixbuf: drop unused imports, drop new() function in favor of simply relying on Default

--- figments/src/pixbuf.rs
+++ figments/src/pixbuf.rs
@@ -1,20 +1,12 @@
 #![doc = "Pixel buffer types"]
-use core::iter::Enumerate;
-use core::ops::IndexMut;
-use core::fmt::Debug;
-use core::slice::Iter;
-use crate::prelude::{CoordinateSpace, Coordinates};
-use crate::render::Sample;
 use crate::pixels::*;
 
 /// Types that provide access to a buffer of pixels, which may or may not be hardware based
 /// 
 /// This trait requires [IndexMut] so you can acccess individual pixels by index
-pub trait Pixbuf: Send + Debug {
+pub trait Pixbuf {
     /// The underlying hardware pixel type
     type Format: HardwarePixel;
-    /// Creates a new Pixbuf that may or may not contain default pixel values (eg, black)
-    fn new() -> Self;
 
     /// Blanks the pixels, usually to black
     fn blank(&mut self);
@@ -25,9 +17,6 @@ pub trait Pixbuf: Send + Debug {
 
 impl<T: HardwarePixel, const PIXEL_NUM: usize> Pixbuf for [T; PIXEL_NUM] where for<'a> T: 'a {
     type Format = T;
-    fn new() -> Self {
-        [T::default(); PIXEL_NUM]
-    }
 
     fn pixel_count(&self) -> usize {
         self.len()
