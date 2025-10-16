32fbabe0a0d4b3641c63a6846026bc20151b83aa
Victoria Fischer <tdfischer@hackerbots.net>
2025-10-16 14:53:22 +0200

    pixels: docs++

--- figments/src/pixels.rs
+++ figments/src/pixels.rs
@@ -6,7 +6,7 @@ use crate::{liber8tion::interpolate::Fract8, prelude::Fract8Ops};
 pub trait PixelFormat: PixelBlend<Self> + Send + Sync + Copy + Default {}
 impl<T> PixelFormat for T where T: PixelBlend<Self> + Send + Sync + Copy + Default {}
 
-/// Types that can blend the values of two pixels together (eg, overlaying RGBA8 on top of plain RGB8)
+/// Types that can blend the value of another pixel
 pub trait PixelBlend<OverlayPixel: PixelFormat> {
     /// Blend a given pixel as an overlay by a given percentage
     fn blend_pixel(self, overlay: OverlayPixel, opacity: Fract8) -> Self;
